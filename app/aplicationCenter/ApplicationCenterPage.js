a842e58c8d8b2a8336dadd4c4194acfdb5db7c75
jinglan1212@icloud.com <zhangjl@i-counting.cn>
2017-06-07 18:22:49 +0800

    新添加应用中心页面,还未写成型的代码

---
+++ app/aplicationCenter/ApplicationCenterPage.js
@@ -0,0 +1,13 @@
+/**
+ * Created by jinglan on 2017/6/7.
+ */
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    Image,
+    View
+} from 'react-native';
+import commonStyles from '../css/styles';
+import styles from './css/ApplictionCenterPageStyle'
