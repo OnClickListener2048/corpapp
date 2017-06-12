7f664ee303b37748c4bd107783f989e9829ac8c2
GraceJia <jiaxt@i-counting.cn>
2017-06-12 10:14:20 +0800

    跳转我的外勤

--- app/aplicationCenter/ApplicationCenterPage.js
+++ app/aplicationCenter/ApplicationCenterPage.js
@@ -2,7 +2,8 @@
  * Created by jinglan on 2017/6/7.
  */
 import React, {Component} from 'react';
-import {Dimensions} from 'react-native';
+import {Dimensions, TouchableOpacity
+,InteractionManager} from 'react-native';
 
 import {
     Text,
@@ -15,6 +16,7 @@ import TopcenterImgBottomTitleView from '../view/TopcenterImgBottomTitleView';
 import styles from './css/ApplictionCenterPageStyle'
 const window = Dimensions.get('window');
 import Swiper from 'react-native-swiper'
+import MyOutSideWorkPage from "../myOutSideWork/MyOutSideWorkPage";
 
 export const SCREEN_WIDTH = window.width;
 export default class ApplicationCenterPage extends Component{
@@ -32,6 +34,14 @@ export default class ApplicationCenterPage extends Component{
         return(
             <Text style={styles.navbarTitleItemStyle}>应用中心</Text>
         );
+    }
+
+    toMyOutSideWork(){
+        InteractionManager.runAfterInteractions(() => {
+            this.props.navigator.push({
+                component: MyOutSideWorkPage,
+            });
+        });
     }
 
     renderImg(){
@@ -65,11 +75,15 @@ export default class ApplicationCenterPage extends Component{
 
             <View style={styles.applicationViewContainer}>
 
-                <TopcenterImgBottomTitleView applicationTitle='我的外勤'
-                                             applicationImg = {require('../img/field.png')}
-                                             style={{ marginLeft: 15,marginTop: 15, height: 100, width: (SCREEN_WIDTH - 45)/2,alignSelf: 'flex-start',}}
-                                             textStyle={{color: '#ef0c35',  alignSelf: 'flex-end'}}
-                />
+                <TouchableOpacity
+                    onPress={() => {this.toMyOutSideWork()}}
+                >
+                    <TopcenterImgBottomTitleView applicationTitle='我的外勤'
+                                                 applicationImg = {require('../img/field.png')}
+                                                 style={{ marginLeft: 15,marginTop: 15, height: 100, width: (SCREEN_WIDTH - 45)/2,}}
+                                                 textStyle={{color: '#ef0c35',  alignSelf: 'flex-end'}}
+                    />
+                </TouchableOpacity>
                 <TopcenterImgBottomTitleView applicationTitle='CRM'
                                              applicationImg = {require('../img/crm_h.png')}
                                              style={{ marginLeft: 15,marginTop: 15, height: 100, width: (SCREEN_WIDTH - 45)/2, alignSelf: 'flex-start',}}
---
+++ app/myOutSideWork/MyOutSideWorkPage.js
@@ -0,0 +1,17 @@
+/**
+ * Created by edianzu on 2017/6/9.
+ */
+
+import React, { Component } from 'react';
+import {Text} from "react-native";
+
+export default class MyOutSideWorkPage extends Component{
+
+    render() {
+        return(
+            <Text style={{color:'red',fontSize:15,}}>
+                我的外勤
+            </Text>
+        );
+    }
+}
