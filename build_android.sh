8364f35b8d5878861fed44cb3b1c9d0927120d04
LiuChangjiong <beansoft@126.com>
2017-07-01 12:29:28 +0800

    更新打包脚本

--- build_android.sh
+++ build_android.sh
@@ -1,4 +1,4 @@
 rm -f android/app/src/main/assets/index.android.bundle
-react-native bundle --platform android --entry-file index.android.js --reset-cache --bundle-output android/app/src/main/assets/index.android.bundle --dev false --assets-dest android/app/src/main/res/
+/usr/local/bin/node /usr/local/lib/node_modules/react-native-cli bundle --platform android --entry-file index.android.js --reset-cache --bundle-output android/app/src/main/assets/index.android.bundle --dev false --assets-dest android/app/src/main/res/
 rm -f android/app/build/outputs/apk/app-release.apk
 cd android && ./gradlew assembleRelease
