69da26ffc18b6f2b2adb6141ef6df61e40667432
LiuChangjiong <beansoft@126.com>
2017-07-06 22:15:33 +0800

    打包更新

--- app/user/LoginPage.js
+++ app/user/LoginPage.js
@@ -154,6 +154,7 @@ export default class LoginPage extends Component {
             apis.sendVerifyCode(this.state.mobile, this.state.vCodeInputValid ? this.state.vCode : null).then(
                 (responseData) => {
                     Toast.show('短信验证码已发送');
+
                     // Toast.show('测试环境短信验证码:' + responseData.msg);
                     // Toast.show('测试环境短信验证码 ' + responseData.msg,
                     //     {position: Toast.positions.TOP, duration: Toast.durations.LONG, backgroundColor: 'green'});
