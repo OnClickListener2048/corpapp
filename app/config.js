eebf6b95b25a7cd4d133c174c85520b9ac05fae7
GraceJia <jiaxt@i-counting.cn>
2017-07-10 14:57:27 +0800

    加载图片失败显示

--- app/config.js
+++ app/config.js
@@ -17,11 +17,15 @@ let {width, height} = Dimensions.get('window');
 export const SCREEN_WIDTH = width;
 export const SCREEN_HEIGHT = height;
 // TODO 线上API服务器接口地址
-export let SCHEME = 'http';
-export let DOMAIN_API = '192.168.100.234:8081';
+// export let SCHEME = 'http';
+export let SCHEME = 'https';
+// export let DOMAIN_API = '192.168.100.234:8081';
+export let DOMAIN_API = 'x-crm.i-counting.cn';
 if(DEBUG) {// 测试环境变量, 上线时应删除
-    SCHEME = 'http';
-    DOMAIN_API = '192.168.100.234:8081';
+    // SCHEME = 'http';
+    SCHEME = 'https';
+    // DOMAIN_API = '192.168.100.234:8081';
+    DOMAIN_API = 'x-crm.i-counting.cn';
 }
 
 export let API_BASE_URL = `${SCHEME}://${DOMAIN_API}`;// API服务基础地址
