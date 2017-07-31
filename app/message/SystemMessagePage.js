3cb7ba61276192210df40875952bc2597e7776b9
LiuChangjiong <beansoft@126.com>
2017-07-31 19:10:58 +0800

    判断系统通知内容是否包含HTML标签, 包含就显示网页, 否则显示旧的文本内容

--- app/message/SystemMessagePage.js
+++ app/message/SystemMessagePage.js
@@ -10,7 +10,8 @@ import {
     ScrollView,
     Dimensions,
     TouchableOpacity,
-    InteractionManager
+    InteractionManager,
+    WebView
 } from 'react-native';
 import BComponent from '../base';
 
@@ -22,14 +23,7 @@ export default class SystemMessagePage extends BComponent{
 
     constructor(props) {
         super(props);
-        this.state = {
-            contentJson:this.props.contentJson,
-            contentStr:'',
-
-        };
         this.stepsArr = [];
-        this.info;
-
     }
 
     static propTypes = {
@@ -39,24 +33,51 @@ export default class SystemMessagePage extends BComponent{
 
 
     render() {
-        const {contentJson} = this.state;
+        // const contentJson = '<!DOCTYPE html>\n' +
+        // '<html>\n' +
+        // '<head>\n' +
+        // '    <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0" />\n' +
+        // '</head>\n' +
+        // '<body style="padding:15px;">\n' +
+        // '    <div style="line-height:35px;">王小二：</div>\n' +
+        // '    <div style="line-height:35px;text-indent: 2em;">你好！</div>\n' +
+        // '    <div style="line-height:35px;width:100%;word-break: break-all; word-wrap:break-word;text-indent: 2em;"> 你在2017年07月20日的<u style="color:red;text-decoration:solid;">测试测试1公司</u>的<u style="color:red;text-decoration:solid;">通办任务2任务</u> 由<u style="color:red;text-decoration:solid;">腰围</u>取消，交由<u style="color:red;text-decoration:solid;">小风</u>处理，如有任何问题，请联系<u style="color:red;text-decoration:solid;">15010672217</u>！</div>\n' +
+        // '</body>\n' +
+        // '</html>' ;//this.props;
+        const {contentJson} = this.props;
+
+        let isHtml = contentJson.includes("<html>");// 判断是否包含HTML标签, 包含就显示网页, 否则显示旧的文本内容
+
+        console.log('contentJson=' + contentJson);
 
         let callback = this.props.callback;
         if(callback) {
             callback();
         }
 
-        // let content = JSON.parse(contentJson);
-        // console.log('contentJson ===' + content)
-        //
-        // console.log('index ===' + content.content)
 
         return(
             <View style={styles.sysContainer}>
 
-                <Text
+                {isHtml &&
+                    <WebView bounces={false}
+                    scalesPageToFit={true}
+                    source={{html: contentJson}}
+                    style={{flex: 1}}
+                    >
+                    </WebView>
+                }
+                {!isHtml &&
+                    <Text
                     textAlign='left'
-                    style={[{fontSize: 15,marginTop: 15, marginLeft : 15,marginRight : 15 , color : '#323232'}] }>{contentJson}</Text>
+                    style={[{
+                    fontSize: 15,
+                    marginTop: 15,
+                    marginLeft: 15,
+                    marginRight: 15,
+                    color: '#323232'
+                }]}>{contentJson}</Text>
+                }
             </View>
         );
     }
