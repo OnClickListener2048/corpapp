eeb2d2c742c2823b8977ac50909848b16e884302
liufei <liufei@icounting.cn>
2017-08-22 11:49:06 +0800

    测试MyOutSideWorkItem

--- app/outWork/myOutSideWork/__tests__/MyOutSideWorkItemPage-test.js
+++ app/outWork/myOutSideWork/__tests__/MyOutSideWorkItemPage-test.js
@@ -1,3 +1,51 @@
 /**
  * by liufei on 2017-8-21
  */
+
+import 'react-native';
+import React from 'react';
+import MyOutSideWorkItemPage from '../MyOutSideWorkItemPage'
+import {shallow} from 'enzyme';
+import fetchMock from 'fetch-mock';
+
+
+const navigator = Object.create(null);
+
+navigator.setOnNavigatorEvent = jest.fn();
+navigator.setButtons = jest.fn();
+navigator.callback = jest.fn();
+
+const wrapper = shallow(
+    <MyOutSideWorkItemPage navigator={navigator}/>
+
+);
+let instance = wrapper.instance();
+
+// 修正 Invariant Violation: Native module cannot be null.
+jest.mock('NetInfo', () => {
+    return {
+        isConnected: {
+            fetch: () => {
+                return new Promise((accept, resolve) => {
+                    accept(true);
+                })
+            },
+            addEventListener: jest.fn()
+        }
+    }
+});
+
+
+it('MyOutSideWorkItemPage 重复点击', (done) => {
+    instance.props.allList==='all'
+    jest.useFakeTimers();
+
+    instance._press(null);
+
+    setTimeout(() => {
+        instance._press(1);
+    }, 1000);
+
+    jest.runAllTimers(null);
+    expect(instance.state.canClickBtn, false);
+});
