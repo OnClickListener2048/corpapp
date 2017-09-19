2c1c90d13882ea796b45f3619d16f2a581f031ec
BeanSoft <beansoft@126.com>
2017-09-19 13:55:48 +0800

    修复 jest 测试, 请运行yarn

--- setupJest.js
+++ setupJest.js
@@ -15,3 +15,8 @@ jest.mock('NetInfo', () => {
 
 jest.mock('react-native-device-info');
 jest.mock('react-native-alert');
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
