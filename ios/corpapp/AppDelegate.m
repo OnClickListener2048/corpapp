caf8f27e0c5e35ae9d438e6f3442c106b682a324
BeanSoft <beansoft@126.com>
2017-11-01 14:31:37 +0800

    更新版本号, 避免codepush

--- ios/corpapp/AppDelegate.m
+++ ios/corpapp/AppDelegate.m
@@ -77,9 +77,9 @@ static BOOL isProduction = true;  //填写isProdurion  平时测试时为false �
   NSURL *jsCodeLocation;
 
 #ifdef DEBUG
-//  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+ jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
   
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  //jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
 #else
   jsCodeLocation = [CodePush bundleURL];
