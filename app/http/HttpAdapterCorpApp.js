35eceeb9ffac0ac19ea3be0578fe50902ebc17f5
BeanSoft <beansoft@126.com>
2017-11-27 16:01:10 +0800

    解决HTTP token处理问题

--- app/http/HttpAdapterCorpApp.js
+++ app/http/HttpAdapterCorpApp.js
@@ -10,6 +10,13 @@ export default class HttpAdapterCorpApp extends HttpAdapter {
     async modifyHeaders (headers) : Object {
         let finalHeaders = new Headers();
         finalHeaders.append('userAgent', 'corpapp'); // TODO 登录时的头信息, userAgent
+        try {
+            let token = await UserInfoStore.getUserToken();
+            console.log('modifyHeaders token', token);
+            finalHeaders.append('Authorize', token? token : '0');
+        } catch (error) {
+        }
+
         if(headers) {
             // 获取 headers 内所有的 key
             let headersKeyArray = Object.keys(headers);
@@ -46,18 +53,38 @@ export default class HttpAdapterCorpApp extends HttpAdapter {
 
     // 通用的处理响应报文的业务方法
     async handleResponse(responseJson): Object {
+        console.log('handleResponse:', responseJson);
         if(responseJson !== undefined && responseJson.hasOwnProperty('success') &&
             responseJson.hasOwnProperty('code')) {
             if (responseJson.success === true && responseJson.code === 200) {
                 return responseJson;
-            } else if (responseJson.success === false && responseJson.code === 40000003) {
-                // Token无效时自动跳转到登录页
-                DeviceEventEmitter.emit('goLoginPage', true);
+            } else if (responseJson.success === false && responseJson.code === 500) {
+                if(responseJson.msg && responseJson.msg .includes('用户已过期')) {
+                    // Token无效时自动跳转到登录页
+                    DeviceEventEmitter.emit('goLoginPage', true);
+                }
                 return Promise.reject(responseJson);
             }
         }
         console.log("后台响应报文有误");
         return Promise.reject(responseJson);
+    }
+
+    /**
+     * 处理默认的Http错误信息, 确保msg不为空, 子类可以覆盖此行为.
+     * @param response Response对象
+     * @returns {{code: *, msg: *}}
+     */
+    makeErrorMsg (response) : Object {
+        let json = super.makeErrorMsg(response);
+        let {status, statusText} = response;
+
+        if(status === 403) {
+            // 403 转向登录页面
+            DeviceEventEmitter.emit('goLoginPage', true);
+        }
+
+        return json;
     }
 
     isConnected() : boolean {
