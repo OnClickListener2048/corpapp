157224cb9126fa30918563f6cacff958f533cf20
BeanSoft <beansoft@126.com>
2017-12-11 10:27:44 +0800

    erp.pilipa.cn 临时包

--- app/config.js
+++ app/config.js
@@ -20,7 +20,7 @@ export const SCREEN_HEIGHT = height;
 export let SCHEME = 'https';
 
  // export let DOMAIN_API = 'app.i-counting.cn';
-export let DOMAIN_API = 'x-crm.i-counting.cn';
+export let DOMAIN_API = 'erp.pilipa.cn';
 // if(DEBUG) {// 测试环境变量, 上线时应删除
 //     SCHEME = 'https';
     // DOMAIN_API = '192.168.100.234:8081';
